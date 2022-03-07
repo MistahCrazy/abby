8c909a9fab8148b28847384f6f54a2b6d89d57ea
Patrick <patreeek@protonmail.com>
2022-03-07 12:56:05 -0500

    cleaned up

--- cogs/status.py
+++ cogs/status.py
@@ -1,29 +1,46 @@
 import discord
 from discord.ext import tasks, commands
 from random import choice
+from typing import Tuple
+from functools import cached_property
 
 from utils.exchange import get_exchange_rate, currencies
 
 class Status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.loop = self.bot.loop
-        self.select_from = tuple(k for k, v in currencies.items() if v['crypto'] or k == 'RUB')
-        self.select_to = tuple(k for k, v in currencies.items() if not v['crypto'] and k != 'RUB')
+        self._last_exchange = ('', '')
 
         self.random_status.start()
 
     def cog_unload(self):
         self.random_status.stop()
 
+    @cached_property
+    def _exchange_from(self):
+        return tuple(k for k, v in currencies.items() if v['crypto'] or k == 'RUB')
+
+    @cached_property
+    def _exchange_to(self):
+        return tuple(k for k, v in currencies.items() if not v['crypto'] and k != 'RUB')
+
+    def get_random_exchange(self) -> Tuple[str, str]:
+        ex = ('BTC', 'USD')
+
+        for _ in range(7):
+            ex = (choice(self._exchange_from), choice(self._exchange_to))
+
+            if ex[0] != self._last_exchange[0]:
+                break
+
+        return ex
+
     @tasks.loop(minutes=2, seconds=30)
     async def random_status(self):
-        sf = choice(self.select_from)
-        st = choice(self.select_to)
-        val = get_exchange_rate(sf, st)
+        cf, ct = self.get_random_exchange()
 
         activity = discord.Activity(
-            name=f'{currencies[sf]["symbol"]} at {val}',
+            name=f'{currencies[cf]["symbol"]} at {get_exchange_rate(cf, ct)}',
             type=discord.ActivityType.watching
         )
 
