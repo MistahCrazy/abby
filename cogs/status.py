b0383629368e59e256fd4393b1907443524c0552
Patrick <patreeek@protonmail.com>
2022-03-07 10:58:01 -0500

    more fun stuff

--- cogs/status.py
+++ cogs/status.py
@@ -1,23 +1,26 @@
 import discord
 from discord.ext import tasks, commands
+from random import choice
 
-from utils.shite import get_exchange_loop()
+from utils.exchange import get_exchange_loop
 
 class Status(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.loop = self.bot.loop
-        self.ruble_list = iter()
-        self.bitcoin_list = iter(get_bitcoin_list())
+        self.exchange_loops = (
+            iter(get_exchange_loop('RUB', '₽ at ')),
+            iter(get_exchange_loop('BTC', '₿ at '))
+        )
         self.random_status.start()
 
     def cog_unload(self):
         self.random_status.stop()
 
-    @tasks.loop(minutes=5)
+    @tasks.loop(minutes=1)
     async def random_status(self):
         activity = discord.Activity(
-            name=f'₿ at {next(self.bitcoin_list)}',
+            name=next(choice(self.exchange_loops)),
             type=discord.ActivityType.watching
         )
 
