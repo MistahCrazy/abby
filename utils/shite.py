6133d1c601f8b4efb4519ebf14e24e7b0be78ba1
Patrick <patreeek@protonmail.com>
2022-03-07 10:01:18 -0500

    restructured

--- utils/shite.py
+++ utils/shite.py
@@ -2,6 +2,7 @@ import time
 import requests
 from random import shuffle
 from collections import UserList
+from functools import partial
 
 class GroundhogDay(UserList):
     def __iter__(self):
@@ -9,10 +10,12 @@ class GroundhogDay(UserList):
         return self
 
     def __next__(self):
-        if self.i >= len(self.data) - 1: self.i = 0
+        self.i %= len(self.data)
+        value = self.data[self.i]()
         self.i += 1
-        return self.data[self.i]()
-        
+
+        return value
+
 class to_cache_or_not_to_cache(object):
     def __init__(self, seconds):
         self.interval = seconds 
@@ -42,69 +45,64 @@ def get(url, **params):
 
     return http.json()
 
-def crypto_values(currency_code:str = 'BTC') -> dict:
-    url = f'https://api.coinbase.com/v2/exchange-rates'
+def get_exchange_rate(currency_from:str = 'BTC', currency_to: str = 'USD') -> float:
+    try:
+        url = f'https://api.coinbase.com/v2/exchange-rates'
+        res = get(url, currency=currency_from)['data']
+    except KeyError:
+        raise ValueError(f'unknown currency code "{currency_from}"')
 
-    return get(url, currency=currency_code)['data']
+    try:
+        return round(float(res['rates'][currency_to]), 2)
+    except KeyError:
+        raise ValueError(f'unknown currency code "{currency_to}"')
 
 def swap_punc(text: str) -> str:
     return text.replace('.', '|poopie|').replace(',', '.').replace('|poopie|', ',')
 
-# Will all the locale stuff get me into trouble?
-def get_bitcoin_usd() -> str:
-    value = float(crypto_values('BTC')['rates']['USD'])
+def get_exchange_usd(currency_from) -> str:
+    value = get_exchange_rate(currency_from, 'USD')
     return f'${value:,.2f}'
 
-def get_bitcoin_gpd() -> str:
-    value = float(crypto_values('BTC')['rates']['GBP'])
+def get_exchange_gbp(currency_from) -> str:
+    value = get_exchange_rate(currency_from, 'GBP')
     return f'£{value:,.2f}'
 
-def get_bitcoin_eur() -> str:
-    value = float(crypto_values('BTC')['rates']['EUR'])
+def get_exchange_eur(currency_from) -> str:
+    value = get_exchange_rate(currency_from, 'EUR')
     return swap_punc(f'{value:,.2f} €')
 
-def get_bitcoin_dkk() -> str:
-    value = float(crypto_values('BTC')['rates']['DKK'])
+def get_exchange_dkk(currency_from) -> str:
+    value = get_exchange_rate(currency_from, 'DKK')
     return swap_punc(f'{value:,.2f}') + ' kr.'
 
-def get_bitcoin_php() -> str:
-    value = float(crypto_values('BTC')['rates']['PHP'])
+def get_exchange_php(currency_from) -> str:
+    value = get_exchange_rate(currency_from, 'PHP')
     return f'₱{value:,.2f}'
 
-def get_bitcoin_ars() -> str:
-    value = float(crypto_values('BTC')['rates']['ARS'])
+def get_exchange_ars(currency_from) -> str:
+    value = get_exchange_rate(currency_from, 'ARS')
     return f'AR${value:,.2f}'
 
-def get_bitcoin_cad() -> str:
-    value = float(crypto_values('BTC')['rates']['CAD'])
+def get_exchange_cad(currency_from) -> str:
+    value = get_exchange_rate(currency_from, 'CAD')
     return f'CA${value:,.2f}'
 
-def get_bitoin_all() -> dict:
-    return {
-        'USD': get_bitcoin_usd() + '   ',
-        'GPD': get_bitcoin_gpd() + '   ',
-        'EUR': get_bitcoin_eur() + ' ',
-        'CAD': get_bitcoin_cad() + '   ',
-        'DKK': get_bitcoin_dkk(),
-        'PHP': get_bitcoin_php() + '   ',
-        'ARS': get_bitcoin_ars() + '   '
-    }
-
-def get_bitcoin_list() -> GroundhogDay:
+def get_exchanges_btc(currency_from) -> GroundhogDay:
     values = GroundhogDay([
-        get_bitcoin_usd,
-        get_bitcoin_gpd,
-        get_bitcoin_eur,
-        get_bitcoin_dkk,
-        get_bitcoin_php,
-        get_bitcoin_ars,
-        get_bitcoin_cad
+        partial(get_exchange_usd, currency_from),
+        partial(get_exchange_gbp, currency_from),
+        partial(get_exchange_eur, currency_from),
+        partial(get_exchange_dkk, currency_from),
+        partial(get_exchange_php, currency_from),
+        partial(get_exchange_ars, currency_from),
+        partial(get_exchange_cad, currency_from)
     ])
-
     shuffle(values)
+
     return values
 
 if __name__ == '__main__':
-    for i in get_bitcoin_list():
+    for i in get_exchanges_btc('BTC'):
        print(i)
-       time.sleep(0.25)
+       time.sleep(0.5)
